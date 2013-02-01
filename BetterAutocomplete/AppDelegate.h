acd795e1ff4ef36603387da93fb5c73b7d8c8709
Pieter Jongsma <pieter.jongsma@gmail.com>
2013-02-01 21:15:34 +0100

    Initial Commit

---
+++ BetterAutocomplete/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BetterAutocomplete
+//
+//  Created by Pieter Jongsma on 01-02-13.
+//  Copyright (c) 2013 Pieter Jongsma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BetterAutocomplete/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BetterAutocomplete
+//
+//  Created by Pieter Jongsma on 01-02-13.
+//  Copyright (c) 2013 Pieter Jongsma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
